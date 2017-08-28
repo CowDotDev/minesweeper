f201cfb67f247f8f2b0285b5ac7aee943b1fe653
Zach Case <Coworado@gmail.com>
2017-08-28 22:44:13 +0100

    Day 10

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,13 +1,39 @@
-const printBoard = (board) => {
-    console.log('Current Board: ');
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberofColumns) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        board.push([]);
+        for(let j = 0; j < numberofColumns; j++) {
+            board[i].push(' ');
+        }
+    }
+    return board;
 };
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for (let i = 0; i < numberOfRows; i++) {
+        board.push([]);
+        for(let j = 0; j < numberOfColumns; j++) {
+            board[i].push(null);
+        }
+    }
+    // Set Bombs onto Board
+    let numberOfBombsPlaced = 0;
+    while(numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        // Check if a bomb is already set
+        if(board[randomRowIndex][randomColumnIndex] !== 'B') {
+            board[randomRowIndex][randomColumnIndex] = 'B';
+            numberOfBombsPlaced++;
+        }
+    }
 
-let board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
-printBoard(board);
+    return board;
+};
+const printBoard = (board, boardTitle) => console.log(boardTitle+': \n'+board.map((row) => row.join(' | ')).join('\n'));
 
-board[0][1] = '1';
-board[2][2] = 'B';
-printBoard(board);
+let playerBoard = generatePlayerBoard(3,4);
+let bombBoard = generateBombBoard(3,4,5);
+
+printBoard(playerBoard, 'Player Board');
+printBoard(bombBoard, 'Bomb Board');
