d7470b990147eb23e5138bc15b5009cf5eae8242
Zach Case <Coworado@gmail.com>
2017-08-28 22:09:41 +0100

    Finish Day 7

---
+++ dist/minesweeper.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// Constants
+var blankLine = "  |   |  ";
+var guessLine = "1 |   |  ";
+var bombLine = "  | B |  ";
+
+// Create an Empty Game Board
+console.log("This is what an empty board would look like:");
+console.log(blankLine);
+console.log(blankLine);
+console.log(blankLine);
+
+// Create a Simulated Game Board with Guesses and Bombs
+console.log("This is what a board with a guess and an bomb on it would look like:");
+console.log(guessLine);
+console.log(bombLine);
+console.log(blankLine);
